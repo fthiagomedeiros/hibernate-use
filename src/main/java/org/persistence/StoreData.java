e583448fcf724e5220454f4286b9ba1d6731236d
Francisco Thiago <fthiagomedeiros@gmail.com>
2020-10-31 00:15:22 -0300

    removing boilerplate code

--- src/main/java/org/persistence/StoreData.java
+++ src/main/java/org/persistence/StoreData.java
@@ -4,42 +4,33 @@ import org.domain.Person;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
 public class StoreData {
-    private static SessionFactory factory;
-    public static void main( String[] args )
-    {
-        StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
-        Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();
+    public static void main( String[] args ) {
+//        StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+//        Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();
         Configuration configuration = new Configuration().configure();
         configuration.addAnnotatedClass(Person.class);
         SessionFactory factory = configuration.buildSessionFactory();
-
-//        SessionFactory factory = meta.getSessionFactoryBuilder().build();
 
         Session session = factory.openSession();
         Transaction t = session.beginTransaction();
 
         Person e1 = new Person();
-        e1.setId(1);
         e1.setName("Gaurav1");
 
         Person e2 = new Person();
-        e2.setId(2);
         e1.setName("Gaurav2");
 
-
         session.save(e1);
-        System.out.println("successfully saved" + e1.getId());
+
+        System.out.println("e1_id" + e1.getId());
         session.save(e2);
         t.commit();
 
-        System.out.println("successfully saved" + e1.getId() + "" + e2.getId());
+        session.createQuery("from Person");
+
         factory.close();
         session.close();
     }
